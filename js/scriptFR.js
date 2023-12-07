bf437ca791e6319cdadc1ca8889c10181c4da2f0
Thomas Prévost <7011145@gmail.com>
2023-12-07 22:35:58 +0100

    Add script

--- js/scriptFR.js
+++ js/scriptFR.js
@@ -1,8 +1,12 @@
 monogatari.script ({
     'Français':{
         'Start': [
-            'Salut je suis en fraçais',
-            'jump other'
+            'y COP city, 2023...',
+            'y Il y a bien longtemps, dans une galaxie pas si lointaine, une ombre insidieuse s\'étendait sur une planète autrefois prospère. Les dirigeants politiques corrompus et les industriels sans scrupules, unis par leur soif insatiable de pouvoir et de profit, avaient jeté leur sinistre emprise sur la destinée de cette planète.',
+            'y Pendant des décennies, ces méchants industriels avaient exploité les ressources naturelles sans relâche, émettant d\'énormes quantités de carbone dans l\'atmosphère, plongeant la planète dans un chaos écologique. Les dirigeants politiques, complices de cette entreprise dévastatrice, tournaient un regard aveugle sur les avertissements des scientifiques et des défenseurs de l\'environnement, préférant sacrifier l\'avenir au nom de la cupidité présente.',
+            'y Les cieux se trouvaient obscurcis par les fumées toxiques des usines, et la terre était étreinte par un étau de pollution. Les habitants de cette planète jadis luxuriante luttaient pour leur survie, tandis que les puissants se prélassaient dans leurs tours d\'ivoire, ignorant les cris désespérés de la nature.',
+            'y Mais au milieu de cette obscurité, une lueur d\'espoir commençait à émerger. Des rebelles de l\'environnement se levaient, déterminés à renverser le règne destructeur de ces méchants industriels et dirigeants politiques. La bataille pour restaurer l\'équilibre écologique et sauver la planète était sur le point de commencer, et la force de la nature elle-même se ralliait à ceux qui résistaient à l\'ombre grandissante.',
+            'y L\'avenir de cette planète était en suspens, et le destin de millions reposait sur la capacité des héros à vaincre les forces du côté obscur de l\'industrialisation et de la politique. La galaxie observait avec une inquiétude croissante, se demandant si la lumière de la raison et de la responsabilité écologique pouvait triompher de l\'obscurité cupide qui menaçait d\'engloutir cette planète autrefois éclatante.',
         ],
         'other': [
             'Whouuuuhouu',
