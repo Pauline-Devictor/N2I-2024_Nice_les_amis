04db85feedda5300b3f0abc6e48561281feda4ed
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2023-12-07 23:55:20 +0100

    Préparation des 5 scenes du jeu

--- js/script.js
+++ js/script.js
@@ -6,13 +6,26 @@ monogatari.script ({
 	'English':{
 		'Start': [
 			'Hi, welcome to your first Visual Novel with Monogatari.',
-			'jump other'
+			'jump Scene1'
 		],
 		'Scene1': [
 
+			'jump Scene2'
 		],
-		'other': [
-			'Another Label!',
+		'Scene2': [
+			'Whouuuuhouu',
+			'jump Scene3'
+		],
+		'Scene3': [
+			'Whouuuuhouu',
+			'jump Scene4'
+		],
+		'Scene4': [
+			'Whouuuuhouu',
+			'jump Scene5'
+		],
+		'Scene5': [
+			'Whouuuuhouu',
 			'end'
 		]
 	}
