b62edaa07fcae137cc48ecac596845048a6682f0
Thomas Prévost <7011145@gmail.com>
2023-12-08 01:19:20 +0100

    Add backgrounds

--- js/scriptFR.js
+++ js/scriptFR.js
@@ -4,6 +4,9 @@ monogatari.assets ('scenes', {
     'emir_banknotes': 'emir_banknotes.png',
     'steampunk_polluted': 'steampunk_polluted.png',
     'burning_earth': 'burning_earth.png',
+    'whistleblower': 'whistleblower.png',
+    'riot': 'riot.png',
+    'trial': 'trial.png',
 });
 
 monogatari.script ({
@@ -72,6 +75,7 @@ monogatari.script ({
             'end'
         ],
         'Scene2':[
+            'show background whistleblower',
             'La révélation des preuves crée une opposition croissante entre les écologistes, et Industria Corp, qui lutte pour maintenir son projet en dépit de la mauvaise publicité.',
             'De son côté {{player.name}} a été invité par de nombreuses chaines télévisées pour parler de ses découvertes et a eu la chance de rencontrer un "mentor"' +  "de l'écologie pour l'accompagner en public.",
             'Ce mentor,' + "c'est Jean-Marc JanKenobi, un personnage bien connu de la région PACA dans laquelle se situe la ville de COPcity. Néanmoins, la révélation de ces documents confidentiels comprométants n'a pas été sans conséquences, Alex a aussi été trainé en justice par Industria Corp",
@@ -101,11 +105,13 @@ monogatari.script ({
             'show background climate_activists',
             'Regardez toutes ces pubs ! Toutes veulent nous inciter à toujours consommer plus ! A manger plus de viandes ! A exploiter plus de populations vulnérables et sous payées ! Tout cela pour quoi ? Pour gagner de' + " l'argent sur notre dos tout en polluant notre pauvre planète ! Quel est le sens d'avoir accès à tous ces objets si nous fonçons vers notre autodestruction ?",
             "Ainsi vous exprimez-vous devant les caméras qui passent devant une de vos manifestations.",
+            'show background riot',
             "2 mois se sont écoulés depuis le début de vos manifestations avec Jean-Marc JanKenobi. Et bien que vous ayez refusé l'offre de votre ancien patron, vous n'avez pas eu de nouvelles depuis, qui sait ce qu'il peut être en train de préparer ?",
             "Un matin, en vous préparant comme à votre habitude à aller militer pour le climat et pour votre cause, vous découvrez avec horreur de nombreuses notifications sur votre téléphone.",
             "Vous ne comprenez pas ce qu'il se passe et vous voyez un nombre incalculable d'appels manqués sur votre téléphone. Tandis que vous essayez de comprendre ce qu'il se passe, vous entendez quelqu'un qui sonne à votre porte, c'est votre mentor.",
             "Enfin ! Vous ne vous rendez pas compte de ce qu'il se passe dehors, tout l'internet s'embrase ! ",
             "Mais que se passe-t-il ?",
+            'show background trial',
             "Industria Corp contre attaque. Ils ont fait de nombreux posts sur les réseaux sociaux pour te diffamer, ils ont réussi à réunir de nombreuses fausses preuves sur des crimes que tu aurais soit-disant commis. Tu es accusé tout d'abord de harcèlement, de racisme, de vol à multiple reprise, le tout au sein de l'entreprise. Tout se répandant à une vitesse folle car personne ne pense à vérifier les informations ! ",
             "Bien évidement de nombreuses personnes continuent de te soutenir mais toutes celles qui n'étaient pas au courant de la première affaire se tourne contre toi ! Il faut réagir !",
             "Tu pourras faire une conférence de presse en démontrant les impacts de notre mode de vie, ce qui serait une solution pacifique ou au contraire opter pour une solution offensive et contre attaquer sur les réseaux .",
